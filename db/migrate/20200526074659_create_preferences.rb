3bb09a1ae3ec4caefa80dbb63fe82466c66e95fc
Anil Gurung <anil_gurung15@yahoo.com>
2020-05-26 07:47:26 +0000

    Automatically backed up by Learn

--- db/migrate/20200526074659_create_preferences.rb
+++ db/migrate/20200526074659_create_preferences.rb
@@ -3,6 +3,8 @@ class CreatePreferences < ActiveRecord::Migration[5.0]
     create_table :preferences do |t|
       t.boolean :allow_create_songs
       t.boolean :allow_create_artists
+      
+      t.timestamps
     end
   end
 end
